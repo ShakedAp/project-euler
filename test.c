05c1058c20443fcf612fab6207172f5b9ba92c63
ShakedAp <shaked.apter123@gmail.com>
2023-06-19 20:02:46 +0300

    Finished the test program, test.c
    Finished the test program to test the answers of the code solutions.
    Added comments to describe the functionality.

--- test.c
+++ test.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+#define ANSWERSIZE 256
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
         printf("Usage ./test <problem_number>\n");
+        printf("Example q23/q23 | ./test 23\n");
+
         return 1;
     }
 
+    // Find problem number
     const int problemNumber = atoi(argv[1]);
     if (problemNumber > 838 || problemNumber < 1)
     {
-        printf("Invalid problem number\n");
+        printf("Invalid problem number.\n");
         return 1;
     }
 
+    // Get answer input
+    char buffer[ANSWERSIZE];
+    long double answer;
+    int i = 0;
+    char c;
+    // printf("Enter the solution for problem: ");
+    fgets(buffer, ANSWERSIZE, stdin);
+    answer = strtold(buffer, 0);
+    printf("The answer you entered is: %Lf\n", answer);
+
+    // Open answers file
     const char *answersFilePath = "answers.txt";
     FILE *answersFile = fopen(answersFilePath, "r");
     if (answersFile == NULL)
@@ -26,12 +42,11 @@ int main(int argc, char *argv[])
         return -1;
     }
     
-    char c;
+    // Read the answers file until the data
     const char *pattern = "DATA\n";
-    int i = 0, len = strlen(pattern);
-    
+    int patternLen = strlen(pattern);
 
-    while(i != len && (c = getc(answersFile)) != EOF)
+    while(i != patternLen && (c = getc(answersFile)) != EOF)
     {
         if(c == pattern[i])
             i++;
@@ -39,28 +54,32 @@ int main(int argc, char *argv[])
             i = 0;
     }
 
-    if (i != len)
+    if (i != patternLen)
     {
         printf("Couldn't file the data... \n");
         return -1;
     }
 
-    int answerNumber;
-    char buffer[100];
+    // Read the data
+    int solutionNumber;
 
-    while(fscanf(answersFile, "Problem %3d: %[^\n]\n", &answerNumber, buffer) == 2)
+    while(fscanf(answersFile, "Problem %3d: %[^\n]\n", &solutionNumber, buffer) == 2)
     {
-        if (answerNumber == problemNumber)
+        if (solutionNumber == problemNumber)
         {
             long double value = strtold(buffer, 0);
-            printf("Problem %i, answer: %Lf\n", answerNumber, value);
-            printf("Buffer: %s\n", buffer);
-            answerNumber = -1;
+            printf("The correct answer for problem %d is: %Lf\n", problemNumber, value);
+            if (value == answer)
+                printf("\n\tCORRECT!\n");
+            else
+                printf("\n\tIncorrect.\n");
+
+            solutionNumber = -1;
             break;
         }
     }
 
-    if(answerNumber != -1)
+    if(solutionNumber != -1)
         printf("Coulnd't find a match...\n");
 
     fclose(answersFile);
