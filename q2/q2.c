a69bb9b751bee1f37937bbe66194fdbe213e8d1f
ShakedAp <shaked.apter123@gmail.com>
2023-06-19 20:25:39 +0300

    Solved question 2
    Updated q2/q2.c to contain the solution to the problem.

--- q2/q2.c
+++ q2/q2.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+#define MAXVALUE 4000000
+
 int main()
 {
+    long int sum = 0;
+    long int num1 = 1, num2 = 1;
+    long int temp = 0;
+
+    while (num2 < MAXVALUE)
+    {
+        if(num2 % 2 == 0)
+            sum += num2;
+        temp = num2;
+        num2 += num1;
+        num1 = temp;
+    }
+    
+    printf("%li\n", sum);
 
     return 0;
 }
