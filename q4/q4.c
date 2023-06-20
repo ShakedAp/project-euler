b982420b06a7d7238072ea8570a04750bef496d2
ShakedAp <shaked.apter123@gmail.com>
2023-06-20 09:35:09 +0300

    Solved question 4.
    Added the solution for question 4 int q4/q4.c

--- q4/q4.c
+++ q4/q4.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x);
 
 int main()
 {
+    int i, j;
+    int biggest = 0;
 
+    for(i = 999; i > 99; i--)
+        for(j = 999; j > 99; j --)
+            if(isPalindrome(i*j) && i*j > biggest)
+                biggest = i*j;
+
+    printf("%d\n", biggest);
     return 0;
+}
+
+bool isPalindrome( int x)
+{
+     int num = x;
+     int revNum = 0;
+    while (num > 0) {
+        revNum = revNum * 10 + num % 10;
+        num = num / 10;
+    }
+
+    return x == revNum;
 }
