8d076c06946dfe295d52467316f71cd26162186f
ShakedAp <shaked.apter123@gmail.com>
2023-06-23 13:10:56 +0300

    Solved question 6
    Modified q6/q6.c to contain the solution for the question
    Modified q6/Makefile to build with the math libaray

--- q6/q6.c
+++ q6/q6.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+
+#define MAXNUM 100
 
 int main()
 {
+    long int sum = 0;
+    long int squareSum = 0;
+
+    for(int i = 1; i <= MAXNUM; i++)
+    {
+        sum += i;
+        squareSum += pow(i, 2);
+    }
+
+    printf("%ld\n", (long int) (pow(sum, 2) - squareSum));
 
     return 0;
 }
