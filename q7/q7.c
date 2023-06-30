d8e410351c7188de845ea2b72d967c41124c2dbd
ShakedAp <shaked.apter123@gmail.com>
2023-06-29 23:00:33 -0600

    Added and solved question 7

---
+++ q7/q7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define PRIMEAMOUNT 10001
+
+bool isprime(int);
+
+int main()
+{
+    long int i = 1;
+    int count;
+    for(count = 0; count < PRIMEAMOUNT; i++)
+        if (isprime(i))
+            count++;
+    i--; // The loop increments i one more time after the count reached it's limit
+
+    printf("%ld\n", i);
+
+    return 0;
+}
+
+bool isprime(int x) {
+    for (int d = 2; d * d <= x; d++) {
+        if (x % d == 0)
+            return false;
+    }
+    return x >= 2;
+}
