4930682743392087e5a63dcf0821008518592aff
ShakedAp <shaked.apter123@gmail.com>
2023-07-10 22:35:06 -0600

    Solved question 12

--- q12/q12.c
+++ q12/q12.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+
+int countDivisors(int num);
 
 int main()
 {
+    int triangle_num = 0;
+
+    for(int i = 0; countDivisors(triangle_num) <= 500; triangle_num += ++i);
+
+    printf("%d\n", triangle_num);
 
     return 0;
 }
+
+int countDivisors(int num)
+{
+    int count = 0;
+    for (int i = 1, root = sqrt(num)+1; i < root; i++)
+        if(num % i == 0)
+            count += 2;
+
+    return count;
+}
+
